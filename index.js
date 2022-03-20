01d2fe84cb6b1c024471b9e10103a6b9e98c3554
Saurabh Bhardwaj <saurabhbhardwaj379@gmail.com>
2022-03-20 21:41:01 +0530

    FirstCommit

--- index.js
+++ index.js
@@ -25,7 +25,9 @@ app.post("/listUsers", function (req, res) {
   }
 });
 
-var server = app.listen(process.env.port, function () {
+var PORT = process.env.PORT || 3000;
+
+var server = app.listen(PORT, function () {
   var host = server.address().address;
   var port = server.address().port;
   console.log("Example app listening at http://%s:%s", host, port);
